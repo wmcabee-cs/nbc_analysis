587e9de6c63b5f0e4449959ca35ad4c9b4f07e99
William McAbee <wmcabee@cognitivescale.com>
2019-09-10 15:46:44 -0500

    nbc analysis

---
+++ src/nbc_analysis/main.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+import json
+
+
+def main(infile, outdir):
+    print(infile)
+
+
---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from nbc_analysis.main import main
+
+__author__ = "William McAbee"
+__copyright__ = "William McAbee"
+__license__ = "mit"
+
+
+
