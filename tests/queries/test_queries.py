03a7de2acd7de28498c46759016279d3558ceb57
William McAbee <wmcabee@cognitivescale.com>
2019-10-17 11:47:01 -0500

    Working on queries. Rebuilding environment

--- tests/queries/test_queries.py
+++ tests/queries/test_queries.py
@@ -1,13 +1,18 @@
 import pytest
 from nbc_analysis.utils.config_utils import get_config
-from nbc_analysis.queries.main import main as query
-
+from nbc_analysis.queries.main import main as query, download_test_files
 
 MPID = []
+PARTITION_ID = 'p0000'
 
 
 def _get_config():
     return get_config(config='default')
+
+
+def test_download_files():
+    config = get_config(config='default')
+    download_test_files(config, partition=PARTITION_ID,limit=None)
 
 
 def test_queries():
