8990d9195bd883f58d78fd82df7f1847fe2d0c42
William McAbee <wmcabee@cognitivescale.com>
2019-10-25 15:45:49 -0500

    Finished example query

--- src/nbc_analysis/queries/main.py
+++ src/nbc_analysis/queries/main.py
@@ -2,6 +2,7 @@ from nbc_analysis.utils.debug_utils import retval
 from nbc_analysis.utils.db_utils import get_db
 from nbc_analysis.utils.file_utils import init_dir
 from pathlib import Path
+from toolz import groupby, pluck
 
 import pandas as pd
 
@@ -23,6 +24,16 @@ limit 10
     df['likelihood'] = df.cnt.div(df.cnt.sum()).round(5)
     df = df.drop(columns='cnt')
     obj = df.to_dict(orient='records')
+    obj = list(pluck([category, 'likelihood'], obj))
+    return obj
+
+
+def flatten_dict(obj, category):
+    reader = obj.items()
+    reader = ((tp, cat, v) for (tp, cat), v in reader)
+    obj = {k: {k2: v2[0][2]
+               for k2, v2 in groupby(1, v).items()}
+           for k, v in groupby(0, reader).items()}
     return obj
 
 
@@ -39,21 +50,24 @@ def get_show_by_time_period(engine, category, mpid):
         """
 
     df = pd.read_sql(sql=sql, con=engine, params={'mpid': mpid})
-    dx = pd.crosstab(df.time_period, df[category], values=df.cnt, aggfunc=sum, margins=True, normalize='columns').round(
-        5).fillna('')
+    dx = (pd.crosstab(df.time_period, df[category],
+                      values=df.cnt, aggfunc=sum, normalize='index')
+          .round(5)
+          .fillna(''))
     dx = dx.mask(dx < .001).stack()  # .to_frame('likelihood')
-    dx = dx.to_dict()
-    return dx
+    obj = dx.to_dict()
+    obj = flatten_dict(obj, category=category)
+    return obj
 
 
-def get_ip_info(engine, mpid):
-    categories = 'postal_code, state, city, median_household_income, median_household_costs'
+def get_category_info(engine, mpid, categories):
     sql = f"""
         select {categories} , 
         sum(1) video_end_event_cnt, 
         sum(video_duration_watched) video_duration_watched
     from F_VIDEO_END f
     join DIM_NETWORK net on f.network_key = net.network_key
+    join DIM_PLATFORM net on f.platform_key = net.platform_key
     where 1=1
     and f.mpid = :mpid
 
@@ -71,12 +85,15 @@ def main(config, mpid):
     cfg = config['database']
     engine = get_db(cfg=cfg)
 
+    ip_categories = 'postal_code, state, city, median_household_income, median_household_costs'
+    platform_categories = 'platform, data_connection_type'
     msg = dict(
         mpid=mpid,
+        ip_info=get_category_info(engine, mpid=mpid, categories=ip_categories),
+        platform_info=get_category_info(engine, mpid=mpid, categories=platform_categories),
         shows=get_shows(engine, category='show', mpid=mpid),
         genres=get_shows(engine, category='genre', mpid=mpid),
         shows_by_timeperiod=get_show_by_time_period(engine, category='show', mpid=mpid),
-        genre_by_timeperiod=get_show_by_time_period(engine, category='genre', mpid=mpid),
-        ip_info=get_ip_info(engine, mpid=mpid),
+        genres_by_timeperiod=get_show_by_time_period(engine, category='genre', mpid=mpid),
     )
-    return msg
+    return msg, engine
--- tests/queries/test_queries.py
+++ tests/queries/test_queries.py
@@ -2,7 +2,7 @@ import pytest
 from nbc_analysis.utils.toml_utils import get_config
 from nbc_analysis.queries.main import main as query
 
-MPID = 594429747067128960
+MULTI_GENRES_MPID = 594429747067128960
 TRAVELER_MPID = 2882228801219022660
 
 
@@ -10,6 +10,13 @@ def _get_config():
     return get_config(config='test')
 
 
-def test_queries():
+def test_queries_multi_genre():
+    mpid = MULTI_GENRES_MPID
     config = _get_config()
-    return query(config=config, mpid=TRAVELER_MPID)
+    return query(config=config, mpid=mpid)
+
+
+def test_queries_traveler():
+    mpid = TRAVELER_MPID
+    config = _get_config()
+    return query(config=config, mpid=mpid)
